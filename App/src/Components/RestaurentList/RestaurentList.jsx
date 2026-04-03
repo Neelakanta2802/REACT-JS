c9bdf028c1cbde34d62041650c58a0952cae492e
Avula Neelakanta <avulaneelakanta75@gmail.com>
2026-04-03 16:54:11 +0530

    removed useNavigate

--- App/src/Components/RestaurentList/RestaurentList.jsx
+++ App/src/Components/RestaurentList/RestaurentList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 
 export default function RestaurentList() {
